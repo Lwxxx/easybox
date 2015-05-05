cf9f15ca1bab921482530dde3a4f19db7c4d5017
Lwxxx <lwx169@gmail.com>
2015-05-05 11:47:43 +0800

    [logdog] Skip everything when read log file at first time
    logdog should only care increments in log file, so when
    read log file at first time, we skipped everything.

--- dev-utils/logdog/file_watcher.c
+++ dev-utils/logdog/file_watcher.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <errno.h>
 
+#include "doglog.h"
 #include "file_watcher.h"
 
 void file_watch(char* filename, void (*callback)(char* line))
@@ -11,22 +13,33 @@ void file_watch(char* filename, void (*callback)(char* line))
 	long  pos = 0;
 	char  line_buf[LDG_LINE_MAX] = {0};
 
+	// skip old part of the file
+	fp = fopen(filename, "r");
+	if (NULL == fp)
+	{
+		LDG_ERR("read file %s failed: %s\n", filename, strerror(errno));
+		return;
+	}
+	fseek(fp , 0, SEEK_END);
+	pos = ftell(fp);
+	fclose(fp);
+
+	// watch the file and run callback
 	while (1)
 	{
 		fp = fopen(filename, "r");
 		if (NULL != fp) {
-			
+
 			fseek(fp , pos, SEEK_SET);
 			while (fgets(line_buf, LDG_LINE_MAX, fp)) {
 				callback(line_buf);
 				memset(line_buf, 0, LDG_LINE_MAX);
 				pos = ftell(fp);
 			}
-			
+
 			fclose(fp);
 		}
-		
+
 		sleep(LDG_CHECK_INTERVAL);
 	}
 }
-
